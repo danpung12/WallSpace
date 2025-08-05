1c3a518a1f1c1e0a221c01a82e15c6e338e3b160
201910996 이은총 <aass6863@naver.com>
2025-08-06 03:52:45 +0900

    첫커밋3

--- mybackend/myapp/admin.py
+++
@@ -1,5 +0,0 @@
-# This file is intentionally left empty as we're not using Django admin for this app.
-# If you need to use the admin interface, uncomment and modify the following:
-# from django.contrib import admin
-# from .models import YourModel
-# admin.site.register(YourModel)
--- mybackend/myapp/tests.py
+++
@@ -1,3 +0,0 @@
-# This file is intentionally left empty as we're using pytest for testing.
-# Test files are located in the tests/ directory.
-# To run tests, use: pytest
