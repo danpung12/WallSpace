9c3b88ed366706dacfafc1037bc3120012d28a02
201910996 이은총 <aass6863@naver.com>
2025-08-06 13:07:48 +0900

    1234

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
+      
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
